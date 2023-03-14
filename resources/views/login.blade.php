1a96b1882a0c6a355145a18ed7ea8cf649f0890f
Joey Roeters <joeyroeters@outlook.com>
2023-03-14 14:00:23 +0100

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,89 @@
+@extends("base_clean")
+
+@section('body-content')
+    <style>body { background-image: url('{{ asset("media/auth/bg9.jpg") }}'); } [data-theme="dark"] body { background-image: url('assets/media/auth/bg4-dark.jpg'); }</style>
+
+    <div class="d-flex flex-column flex-column-fluid flex-lg-row">
+        <!--begin::Aside-->
+        <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
+            <!--begin::Aside-->
+            <div class="d-flex flex-center flex-lg-start flex-column">
+                <!--begin::Logo-->
+                <h1 class="text-white fw-bolder fs-3x">
+                    CloudStation
+                </h1>
+                <!--end::Logo-->
+                <!--begin::Title-->
+                <h2 class="text-white fw-normal m-0">Connecting you to the worlds weather</h2>
+                <!--end::Title-->
+            </div>
+            <!--begin::Aside-->
+        </div>
+        <!--begin::Aside-->
+        <!--begin::Body-->
+        <div class="d-flex flex-center w-lg-50 p-10">
+            <!--begin::Card-->
+            <div class="card rounded-3 w-md-550px">
+                <!--begin::Card body-->
+                <div class="card-body p-10 p-lg-20">
+                    <!--begin::Form-->
+                    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="../../demo27/dist/index.html" action="#">
+                        <!--begin::Heading-->
+                        <div class="text-center mb-11">
+                            <!--begin::Title-->
+                            <h1 class="text-dark fw-bolder mb-3">Log in</h1>
+                            <!--end::Title-->
+                            <!--begin::Subtitle-->
+
+                            <!--end::Subtitle=-->
+                        </div>
+                        <!--begin::Heading-->
+                        <!--end::Separator-->
+                        <!--begin::Input group=-->
+                        <div class="fv-row mb-8">
+                            <!--begin::Email-->
+                            <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
+                            <!--end::Email-->
+                        </div>
+                        <!--end::Input group=-->
+                        <div class="fv-row mb-3">
+                            <!--begin::Password-->
+                            <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
+                            <!--end::Password-->
+                        </div>
+                        <!--end::Input group=-->
+                        <!--begin::Wrapper-->
+                        <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                            <div></div>
+                            <!--begin::Link-->
+                            <a href="../../demo27/dist/authentication/layouts/creative/reset-password.html" class="link-primary">Wachtwoord vergeten?</a>
+                            <!--end::Link-->
+                        </div>
+                        <!--end::Wrapper-->
+                        <!--begin::Submit button-->
+                        <div class="d-grid mb-10">
+                            <a type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                <!--begin::Indicator label-->
+                                <span class="indicator-label">Log in</span>
+                                <!--end::Indicator label-->
+                                <!--begin::Indicator progress-->
+                                <span class="indicator-progress">Please wait...
+										<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                <!--end::Indicator progress-->
+                            </a>
+                        </div>
+                        <!--end::Submit button-->
+                        <!--begin::Sign up-->
+                        <div class="text-gray-500 text-center fw-semibold fs-6">Nog geen account?
+                            <a href="../../demo27/dist/authentication/layouts/creative/sign-up.html" class="link-primary">Contacteer administrator</a></div>
+                        <!--end::Sign up-->
+                    </form>
+                    <!--end::Form-->
+                </div>
+                <!--end::Card body-->
+            </div>
+            <!--end::Card-->
+        </div>
+        <!--end::Body-->
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+class Pager {
+
+    public $route1;
+    public $route2;
+    public $title;
+    public $action = false;
+
+    public function __construct(
+        string $route1,
+        string $route2,
+        string $title
+    )
+    {
+        $this->route1 = $route1;
+        $this->route2 = $route2;
+        $this->title = $title;
+    }
+
+    /**
+     * @param bool $action
+     */
+    public function setAction(bool $action): void
+    {
+        $this->action = $action;
+    }
+
+    public static function create(
+        string $route1 = '',
+        string $route2 = '',
+        string $title
+    )
+    {
+        return new Pager($route1, $route2, $title);
+    }
+
+    public function export()
+    {
+        return [
+            'route1' => $this->route1,
+            'route2' => $this->route2,
+            'title' => $this->title,
+            'action' => $this->action
+        ];
+    }
+}
+Route::get('/', function () {
+    return view('login', ['login' => true]);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard', Pager::create('overzicht', '', 'Overzicht')->export());
+});
+
+
+Route::get('/users', function () {
+    return view('users', Pager::create('administratie', 'gebruikers', 'Gebruikers')->export());
+})->name('users');
+
+Route::get('/stations', function () {
+    return view('users', Pager::create('stations', '', 'Weer stations')->export());
+})->name('stations');
+
+Route::get('/analyse', function () {
+    return view('users', Pager::create('Analyseer', '', 'Analyseer stations')->export());
+})->name('analyse');
